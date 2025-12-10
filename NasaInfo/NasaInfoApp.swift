69d6a5c5ce96e721404c7e12cdd7a6e86af028a3
ca5 <catinice@outlook.com>
2025-12-10 21:54:51 +0530

    Initial Commit

---
+++ NasaInfo/NasaInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaInfoApp.swift
+//  NasaInfo
+//
+//  Created by ca5 on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NasaInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
