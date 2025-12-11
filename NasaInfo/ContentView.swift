0a9079e41d35d43aea96dd2ccd0570f2fa82bef7
ca5 <catinice@outlook.com>
2025-12-11 18:00:34 +0530

    navigationTitle inside navigationView
    Signed-off-by: ca5 <catinice@outlook.com>
    
    Change-Id: Ia2df4caa4baf40dbd4da267f0e0f5af3b3d4ad26

--- NasaInfo/ContentView.swift
+++ NasaInfo/ContentView.swift
@@ -20,18 +20,30 @@ struct ContentView: View {
         student(name: "Charlie", age: 21)
     ]
     var body: some View {
-        NavigationStack {
+        NavigationStack{
+            List {
+                ForEach(students, id: \._id){
+                    i in
+                    NavigationLink("Enter \(i.name) detail", value: i)
+                }
+                
+                ForEach(0..<5){
+                    k in
+                    NavigationLink("View number \(k) details", value: k)
+                }
+            }
+            .navigationTitle("MAIN SCREEN")
+            .navigationDestination(for: Int.self) { l in
+                    Text("Hello \(l)")
+                    .navigationTitle("SIDE SCREEN 1")
+            }
+            .navigationDestination(for: student.self) { stu in
+                Text("student details: \(stu.name), \(stu._id)")
+            }
             
-            List(students, id: \._id){
-                i in
-                NavigationLink("Select \(i.name)", value: i)
-            }
-            .navigationDestination(for: student.self) {
-                k in
-                Text("You selected \(k._id) and the name is \(k.name) and age is \(k.age)")
-            }
         }
     }
+    
 }
 
 #Preview {
