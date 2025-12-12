527b29b57225f1c16aa09a4e41dcbf00c2ea5b09
ca5 <catinice@outlook.com>
2025-12-12 19:58:55 +0530

    lazyVgrid and GridView
    Signed-off-by: ca5 <catinice@outlook.com>
    
    Change-Id: I7dca4b9ccefda3a49527a04f3523fd032f1ca136

--- NasaInfo/ContentView.swift
+++ NasaInfo/ContentView.swift
@@ -1,18 +1,20 @@
 import SwiftUI
 
 struct ContentView : View {
+    let gview = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Image(.wallhavenGw767D)
-            .resizable()
-//            .scaledToFit()
-//            .scaledToFill()
-//            .frame(width: 300, height: 300)
-            .containerRelativeFrame(.horizontal) {
-                size, axis in
-                return size * 0.8 // render image 80% of horizontal space available.
+        ScrollView{
+            LazyVGrid(columns: gview)
+            {
+                ForEach(0..<1000){
+                    k in
+                    Text("HII")
+                }
             }
-//            .clipped()
-           
+        }
     }
 }
 #Preview {
